4754e354d079a601b5ab890c7d053601561e12c4
jtdub <amascuba@gmail.com>
2018-05-16 22:59:09 -0500

    update setup and travis

--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@
 
 from setuptools import setup
 from setuptools import find_packages
-from hier_config import __version__
 
 setup(
     name="Hierarchical Configuration",
     description="A router and switch configuration intention tool, used to build remediation configurations.",
-    version=__version__,
+    version=1.0.0,
+    install_requires=[
+        'PyYAML',
+        'Sphinx',
+        'sphinx-rtd-theme',
+    ],
     packages=find_packages(),
     author="Andrew Edwards, Jan Brooks, James Williams",
     email="andrew.edwards@rackspace.com, jan.brooks@rackspace.com, james.williams@rackspace.com",
