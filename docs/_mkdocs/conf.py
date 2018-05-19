161236b685c2e20975e2014317857650dc884fde
jtdub <amascuba@gmail.com>
2018-05-18 21:42:50 -0500

    update docs

--- docs/_mkdocs/conf.py
+++ docs/_mkdocs/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -24,7 +24,7 @@ copyright = '2018, Andrew Edwards, Jan Brooks, James Williams'
 author = 'Andrew Edwards, Jan Brooks, James Williams'
 
 # The short X.Y version
-version = ''
+version = '1.0.0'
 # The full version, including alpha/beta/rc tags
 release = '1.0.0'
 
@@ -79,7 +79,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
