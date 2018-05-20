3b30a1fb394ef03f4ee659fd6e56ee82dfc3f1cc
jtdub <amascuba@gmail.com>
2018-05-19 21:19:25 -0500

    increment version and update classifer in setup

--- hier_config/__init__.py
+++ hier_config/__init__.py
@@ -2,7 +2,7 @@ from hier_config.hc_child import HierarchicalConfiguration
 
 import re
 
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 
 
 class HierarchicalConfigurationRoot(HierarchicalConfiguration):
