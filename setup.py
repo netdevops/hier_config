a9ba44cab0aa38788376995cee0c52d95cdabc7c
jtdub <james.williams@packetgeek.net>
2020-11-11 16:47:45 -0600

    update travis and setup

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     keywords="hier_config",
     python_requires=">=3.7",
     install_requires=["pyyaml", "pytest-runner"],
-    tests_require=["pytest", "pep8", "pytest-cov", "pytest-black"],
+    tests_require=["pytest", "pep8", "pytest-cov", "pytest-black", "pytest-flake8"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
@@ -29,7 +29,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
+        # "Programming Language :: Python :: 3.10",
         "Natural Language :: English",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: System :: Networking",
