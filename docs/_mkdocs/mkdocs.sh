39d9a844c889eee37415fad733f697d27e74fafe
jtdub <james.williams@packetgeek.net>
2020-12-10 11:38:05 -0600

    update docs info

---
+++ docs/_mkdocs/mkdocs.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+function make_docs {
+# create python virtual env
+pip install sphinx sphinx_rtd_theme
+
+# Automatically create code documentation
+sphinx-apidoc -f -o hier_config ../../hier_config
+
+# Remove exisitng code table of contents
+if [ -f hier_config.rst ]; then
+    rm hier_config.rst
+fi
+
+# Create a new code table of contents
+cat > hier_config.rst << EOF
+.. _hier_config:
+
+==================
+Code Documentation
+==================
+
+.. toctree::
+   :maxdepth: 1
+
+   hier_config/modules.rst
+
+EOF
+
+# Create the html files
+make clean
+make dirhtml
+
+# Sync generated documentation to viewable path
+rsync -avz --exclude sphinx-env "${PWD}/_build/dirhtml/" "../../docs"
+
+exit 0
+}
+
+make_docs
+
