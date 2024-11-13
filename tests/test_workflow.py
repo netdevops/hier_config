387f87e014c6d8d80a53ee57507a689a7a564b98
James Williams <amascuba@gmail.com>
2024-11-13 13:30:37 -0600

    update test_workflows linting

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -52,7 +52,9 @@ def test_remediation_config_driver_mismatch() -> None:
     running_config = get_hconfig(Platform.CISCO_IOS, "dummy_config")
     generated_config = get_hconfig(Platform.JUNIPER_JUNOS, "dummy_config")
 
-    with pytest.raises(ValueError, match="The running and generated configs must use the same driver."):
+    with pytest.raises(
+        ValueError, match="The running and generated configs must use the same driver."
+    ):
         WorkflowRemediation(running_config, generated_config)
 
 
@@ -66,6 +68,8 @@ def test_rollback_config_exists(wfr: WorkflowRemediation) -> None:
 def test_rollback_config_reverts_changes(wfr: WorkflowRemediation) -> None:
     # Test if rollback config correctly represents changes needed to revert generated to running
     rollback_config = wfr.rollback_config
-    rollback_text = "\n".join(line.cisco_style_text() for line in rollback_config.all_children_sorted())
+    rollback_text = "\n".join(
+        line.cisco_style_text() for line in rollback_config.all_children_sorted()
+    )
     expected_text = "no vlan 4\nno interface Vlan4\nvlan 3\n  name switch_mgmt_10.0.4.0/24\ninterface Vlan2\n  no mtu 9000\n  no ip access-group TEST in\n  shutdown\ninterface Vlan3\n  description switch_mgmt_10.0.4.0/24\n  ip address 10.0.4.1 255.255.0.0"
     assert rollback_text == expected_text
