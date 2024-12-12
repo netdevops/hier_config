b6cd075399622892bfdc8c62eefa36e7bf784f9a
jtdub <amascuba@gmail.com>
2024-12-11 18:56:31 -0600

    linting

--- hier_config/__init__.py
+++ hier_config/__init__.py
@@ -1,4 +1,4 @@
-from importlib.metadata import version, PackageNotFoundError
+from importlib.metadata import PackageNotFoundError, version
 
 from .child import HConfigChild
 from .constructors import (
