d53d9f63bf0a13f22bdd7013fdd13b2be5ff7b8b
James Williams <amascuba@gmail.com>
2025-01-14 11:36:39 -0600

    ignore UP045

--- hier_config/platforms/hp_procurve/view.py
+++ hier_config/platforms/hp_procurve/view.py
@@ -292,7 +292,7 @@ class HConfigViewHPProcurve(HConfigViewBase):
 
     @property
     def stack_members(self) -> Iterable[StackMember]:
-        """stacking
+        """Stacking
         member 1 type "JL123" mac-address abc123-abc123
         member 1 priority 255
         member 2 type "JL123" mac-address abc123-abc123
