b408bdc71322e206a7c351d2da6c6fbcc4d9aa3d
jtdub <amascuba@gmail.com>
2025-07-09 13:19:37 -0500

    add duplicate child test for xr

---
+++ tests/test_driver_cisco_xr.py
@@ -0,0 +1,44 @@
+from hier_config import get_hconfig_fast_load
+from hier_config.models import Platform
+from tests.conftest import running_config
+
+def test_duplicate_child() -> None:
+    platform = Platform.CISCO_XR
+    running_config = get_hconfig_fast_load(
+        platform,
+        (
+            "route-policy SET_COMMUNITY_AND_PERMIT",
+            "  if destination in (192.0.2.0/24, 198.51.100.0/24) then",
+            "    set community (65001:100) additive",
+            "    pass",
+            "  else",
+            "    drop",
+            "  endif",
+            "end-policy",
+            "",
+            "route-policy SET_LOCAL_PREF_AND_PASS",
+            "  if destination in (203.0.113.0/24) then",
+            "    set local-preference 200",
+            "    pass",
+            "  else",
+            "    drop",
+            "  endif",
+            "end-policy",
+        ),
+    )
+    generated_config = get_hconfig_fast_load(
+        platform,
+        (
+            "route-policy SET_COMMUNITY_AND_PERMIT",
+            "  if destination in (192.0.2.0/24, 198.51.100.0/24) then",
+            "    set community (65001:100) additive",
+            "    pass",
+            "  else",
+            "    drop",
+            "  endif",
+            "end-policy",
+            "",
+        ),
+    )
+    remediation_config = running_config.config_to_get_to(generated_config)
+    assert remediation_config.dump_simple(sectional_exiting=True) == "no route-policy SET_LOCAL_PREF_AND_PASS"
